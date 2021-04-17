fe5c49488f56418d5f8ac8b7c6aa244227061d74
diepunk <81024042+diepunk@users.noreply.github.com>
2021-04-17 15:13:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let A = document.getElementById("mya");
+
+let B = document.getElementById("myb");
+
+
+
+function equal(A,B)
+{
+    A = Number.parseInt("mya");
+    B = Number.parseInt("myb");
+    if (A=B){ return "A=B" }
+    else { return "A!=B" } 
+}
